7aba519baf347e0bf4cac8735e1eee9677cb29a7
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-11-01 23:37:43 +0200

    Added LTO optimization

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
-#include <fmt/chrono.h>
+#include <fmt/core.h>
 
 int main() {
-  using namespace std::literals::chrono_literals;
-  std::string s = fmt::format("I'd rather be {1} than {0}.", "right", "happy");
+  std::string s = fmt::format("The answer is {}.", 42);
   return 0;
 }
